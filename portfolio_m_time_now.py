fb98c396ad75a7ed928da4fd3938645bb506c798
MROK0703DY <164418970+MROK0703DY@users.noreply.github.com>
2026-04-05 22:29:22 +0300

    PF-01 Added index.html, date_now.py

---
+++ portfolio_m_time_now.py
@@ -0,0 +1,28 @@
+from datetime import datetime, date
+
+# 1. Получение текущей даты и времени
+now = datetime.now()
+print(f"\033[1;31m Текущие дата и время: {now} \033[0m")  # Выведет что-то
+# вроде: 2025-12-07 10:30:00.123456
+
+# 2. Получение только текущей даты
+today = date.today()
+print()
+print(f"Только сегодняшняя дата: {today}")  # Выведет: 2025-12-07
+
+# 3. Форматирование даты в строку (например, в ДД.ММ.ГГГГ)
+# %d - день, %m - месяц, %Y - год (полностью)
+formatted_date_ru = today.strftime("%d.%m.%Y")
+print(f"Дата в формате ДД.ММ.ГГГГ: {formatted_date_ru}")  # Выведет: 07.12.2025
+
+# 4. Форматирование в международный стандарт ISO 8601 (YYYY-MM-DD)
+formatted_date_iso = today.isoformat()  # или today.strftime("%Y-%m-%d")
+print(f"Дата в формате ISO 8601: {formatted_date_iso}")  # Выведет: 2025-12-07
+
+# 5. Форматирование с полным названием месяца
+formatted_date_full = today.strftime("%d %B %Y   %A")
+# %B для полного названия месяца (зависит от локали)
+print()
+print(f"\033[1;31m Дата с названием месяца: {formatted_date_full} \033[0m а время то отсутствует :-)"
+      f"")
+# Выведет: 07 December 2025
